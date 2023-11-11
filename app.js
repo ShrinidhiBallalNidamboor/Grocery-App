ca80e5eb0e2707693517224b9973f7931a6b75a5
Shrinidhi <ballalshrinidhi@gmail.com>
2023-11-11 11:46:54 +0530

    .

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const ejs = require("ejs");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 const crypto = require('crypto'); // Import NodeJS's Crypto Module
@@ -338,20 +337,6 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
-
 app.get("/", function(req, res){
   information_farmer.find({}, function (err, info1) {
     if (err){
